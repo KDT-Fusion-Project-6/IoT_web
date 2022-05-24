718abe710e62362921c0c47a5fad1ad75eaacb2f
wjdtjddjq <tjddjq0114@naver.com>
2022-05-24 14:53:16 +0900

    220524 cal_del

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'board',
-    'cal.apps.CalConfig',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,6 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from cal import views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,6 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('board/', include('board.urls')),
-    path('', views.CalendarView.as_view(), name='index'), # '/' 에 해당되는 path
-    path('cal/', include('cal.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

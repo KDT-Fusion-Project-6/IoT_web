a2851fc0cf25c681db4ae1e1a92611ad36d5dd7b
tjddjq0114 <tjddjq0114@naver.com>
2022-05-27 15:14:38 +0900

    use삭제

--- main/settings.py
+++ main/settings.py
@@ -108,11 +108,6 @@ LANGUAGE_CODE = 'ko'
 TIME_ZONE = 'Asia/Seoul'
 
 
-USE_I18N = True
-
-USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 

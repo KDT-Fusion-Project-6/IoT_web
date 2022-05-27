6d8230721d3d98f474f940e06ea6531232a67fe9
tjddjq0114 <tjddjq0114@naver.com>
2022-05-27 11:24:42 +0900

    login,signup(html/views.py/urls,수정, form_error수정,

--- login/urls.py
+++ login/urls.py
@@ -8,6 +8,7 @@ from . import views
 app_name ='login'
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('login/',auth_views.LoginView.as_view(template_name='login/login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
--- login/views.py
+++ login/views.py
@@ -3,11 +3,10 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from login.forms import UserForm
 # Create your views here.
+def index(request):
+    return render(request, 'login/login.html')
 
-def signup(request):
-    """
-    계정생성
-    """
+def signup(request): # 계정생성
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
@@ -15,6 +14,7 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
+
             login(request, user)
             return redirect('index')
     else:

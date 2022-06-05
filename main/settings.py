d05123654c880e08841264170907064d640e2777
Jiwon Lim <onuonu2222@gmail.com>
2022-06-05 23:05:24 +0900

    requirements.py에 pymysql추가

--- main/settings.py
+++ main/settings.py
@@ -85,17 +85,17 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-    # 'default': { # 추가
-    # 'ENGINE': 'django.db.backends.mysql',
-    # 'NAME': 'django_db', # DB명
-    # 'USER': 'python', # 데이터베이스 계정
-    # 'PASSWORD':'python', # 계정 비밀번호
-    # #'HOST':'localhost', # 데이테베이스 IP
-    # 'HOST':'mysql-svc', # rds ep
-    # 'PORT':'3306', # 데이터베이스 port
+    #     'default': { # 추가
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'django_db', # DB명
+    #     'USER': 'python', # 데이터베이스 계정
+    #     'PASSWORD':'python', # 계정 비밀번호
+    #     #'HOST':'localhost', # 데이테베이스 IP
+    #     'HOST':'mysql-svc', # rds ep
+    #     'PORT':'3306', # 데이터베이스 port
     # }
 }
 
